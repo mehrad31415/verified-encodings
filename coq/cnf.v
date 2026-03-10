53806940ac049800345a23f624890ed59fc7ad05
linxifan <linxifan24@gmail.com>
2026-03-10 19:05:29 -0400

    update bug fixed

--- coq/cnf.v
+++ coq/cnf.v
@@ -12,26 +12,25 @@ Import ListNotations.
 Section CNF_Definition.
 
 Variable V : Type. 
-
+Definition cnf := list (clause V). 
  
 
 Hypothesis V_eq_dec : forall x y : V, {x = y} + {x <> y}.
 
 
-Definition cnf := list clause.
-
 Definition assignment := V -> bool.
 
 
 
-Definition eval_literal (τ : assignment) (l : literal) : bool :=
+Definition eval_literal (τ : assignment) (l : literal V) : bool :=
   match l with
   | Pos v => τ v
   | Neg v => negb (τ v)
   end.
 
-Definition eval_clause (τ : assignment) (c : clause) : bool :=
+Definition eval_clause (τ : assignment) (c : clause V) : bool :=
   existsb (eval_literal τ) c.
+
 
 // starting from protected def from line 68
 Definition eval_cnf (τ : assignment) (f : cnf) : bool :=
@@ -43,21 +42,27 @@ Theorem eval_nil : forall τ, eval_cnf τ [] = true.
 Proof. reflexivity. Qed.
 
 Theorem eval_singleton : forall τ c, eval_cnf τ [c] = eval_clause τ c.
-Proof. intros. simpl. apply andb_true_r. Qed.
+Proof. intros. simpl. rewrite andb_true_r. reflexivity. Qed.
 
 Theorem eval_cons : forall τ c f, eval_cnf τ (c :: f) = eval_clause τ c && eval_cnf τ f.
-Proof. intros. simpl. apply creativity, andb_comm. Qed.
+Proof. intros. simpl. rewrite andb_comm. reflexivity. Qed.
+
 
 // from line 79 
 Theorem eval_tt_iff_forall_clause_eval_tt : 
   forall τ f, eval_cnf τ f = true <-> (forall c, In c f -> eval_clause τ c = true).
 Proof.
   intros τ f. induction f as [| c cs IH].
-  - split; simpl; intros; tauto.
-  - rewrite eval_cons, andb_true_iff, IH.
+  - split; simpl; intros; try contradiction; auto.
+  - simpl. rewrite andb_true_iff, IH.
     split; intros.
-    + destruct H. intros c0 [Hinc | Hincs]. subst; auto. auto.
-    + split. apply H. left. auto. intros c0 Hinc. apply H. right. auto.
+    + destruct H as [Hc Hcs].
+      intros c0 [H | H].
+      * subst. assumption.
+      * apply Hcs. assumption.
+    + split.
+      * apply H. left. reflexivity.
+      * intros c0 Hc0. apply H. right. assumption.
 Qed.
 
 
@@ -65,32 +70,38 @@ Theorem eval_ff_iff_exists_clause_eval_ff :
   forall τ f, eval_cnf τ f = false <-> (exists c, In c f /\ eval_clause τ c = false).
 Proof.
   intros τ f. induction f as [| c cs IH].
-  - split; simpl; intros. inversion H. destruct H as [_ [H _]]. inversion H.
-  - rewrite eval_cons, andb_false_iff, IH.
+  - split; simpl; intros.
+    + discriminate.
+    + destruct H as [c [H _]]. inversion H.
+  - simpl. rewrite andb_false_iff, IH.
     split.
     + intros [Hc | Hcs].
       * exists c. split; [left; reflexivity | assumption].
-      * destruct Hcs as [c' [Hinc' Hff']]. exists c'. split; [right; assumption | assumption].
-    + intros [c' [[Hsub | Hinc'] Hff]].
-      * left. subst. assumption.
-      * right. exists c'. auto.
+      * destruct Hcs as [c' [Hinc' Hff']].
+        exists c'. split; [right; assumption | assumption].
+    + intros [c' [[H | H] Hff]].
+      * subst. left. assumption.
+      * right. exists c'. split; assumption.
 Qed.
 
 
 // from line 165
+
 Definition satisfiable (f : cnf) := exists τ, eval_cnf τ f = true.
 
 Theorem satisfiable_of_eval_tt : forall f τ, eval_cnf τ f = true -> satisfiable f.
 Proof. intros f τ H. exists τ. auto. Qed.
 
 
-Definition lit_var (l : literal) : V :=
+Definition lit_var (l : literal V) : V :=
   match l with | Pos v => v | Neg v => v end.
 
-Definition clause_vars (c : clause) : list V :=
+Definition clause_vars (c : clause V) : list V :=
   map lit_var c.
 
-  // from line 126 in lean
+
+// from line 126 in lean
+
 Fixpoint vars (f : cnf) : list V :=
   match f with
   | [] => []
@@ -102,15 +113,17 @@ Theorem mem_vars_iff_exists_mem_clause_and_mem :
   forall v f, In v (vars f) <-> (exists c, In c f /\ In v (clause_vars c)).
 Proof.
   intros v f. induction f as [| c cs IH].
-  - simpl. split; intros. inversion H. destruct H as [_ [H _]]. inversion H.
+  - simpl. split; intros.
+    + contradiction.
+    + destruct H as [c [H _]]. inversion H.
   - simpl. rewrite in_app_iff, IH.
     split.
     + intros [Hv | [c' [Hinc' Hv]]].
-      * exists c. auto.
-      * exists c'. auto.
-    + intros [c' [[Hsub | Hinc'] Hv]].
-      * left. subst. auto.
-      * right. exists c'. auto.
+      * exists c. split; [left; reflexivity | assumption].
+      * exists c'. split; [right; assumption | assumption].
+    + intros [c' [[H | H] Hv]].
+      * subst. left. assumption.
+      * right. exists c'. split; assumption.
 Qed.
 
 
@@ -121,10 +134,11 @@ Definition eqsat (f1 f2 : cnf) := satisfiable f1 <-> satisfiable f2.
 Definition agree_on (τ1 τ2 : assignment) (s : list V) :=
   forall v, In v s -> τ1 v = τ2 v.
 
+
 // from line 268 in lean
 Definition sequiv (f1 f2 : cnf) (s : list V) :=
   forall τ, 
     (exists σ1, eval_cnf σ1 f1 = true /\ agree_on τ σ1 s) <->
     (exists σ2, eval_cnf σ2 f2 = true /\ agree_on τ σ2 s).
 
-End CNF_Definitions.
+End CNF_Definition.
